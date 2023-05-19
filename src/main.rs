637825c2fb57c2c3cc7d10ed9f3b479b732d0420
Davide Ghilardi <ghilardi.davide@gmail.com>
2023-05-19 03:03:17 +0200

    add check version

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,22 @@ use crate::args::{Commands, RunThatCli};
 mod args;
 
 fn main() {
+    check_version();
     let args: RunThatCli = RunThatCli::parse();
 
     match args.subcommand {
         Commands::Run(run_args) => println!("Run {} -- {:?}", run_args.script, run_args.script_args),
+    }
+}
+
+fn check_version() {
+    use update_informer::Check;
+    use update_informer::registry::Crates;
+
+    let name = env!("CARGO_PKG_NAME");
+    let version = env!("CARGO_PKG_VERSION");
+    let informer = update_informer::new(Crates, name, version);
+    if let Some(version) = informer.check_version().ok().flatten()  {
+        println!("New version is available: {}", version);
     }
 }
